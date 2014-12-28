0261ef85a2d7153cd81225cb4c198e86da6f284f
Benjamin Doherty <bjd@bangpound.org>
2014-12-28 01:07:33 -0600

    code style.

--- lib/DrupalHttpDriver.php
+++ lib/DrupalHttpDriver.php
@@ -3,16 +3,16 @@
 class DrupalHttpDriver extends \DebugBar\PhpHttpDriver
 {
     /**
-   * Sets HTTP headers
-   *
-   * @param array $headers
-   */
-  public function setHeaders(array $headers)
-  {
-      foreach ($headers as $name => $value) {
-          drupal_add_http_header($name, $value, true);
-      }
-  }
+     * Sets HTTP headers
+     *
+     * @param array $headers
+     */
+    public function setHeaders(array $headers)
+    {
+        foreach ($headers as $name => $value) {
+            drupal_add_http_header($name, $value, true);
+        }
+    }
 
     public function isSessionStarted()
     {
